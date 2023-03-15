5ced26f05089fb082bed39efe009aba1965c5b73
Santosh Krishnamurthy <san.b350@gmail.com>
2023-03-15 10:21:58 -0500

    Initial Commit

---
+++ proto-magic-grid-animation-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  proto-magic-grid-animation-ios
+//
+//  Created by Santosh Krishnamurthy on 3/15/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
